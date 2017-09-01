da721d699a9e7809a02964092c1b7757455fdf89
pc <pc@CS-T-Z0002>
2017-09-01 15:18:01 +0800

    第一次

--- src/main/java/com/gobravery/wind/server/UploadService.java
+++
@@ -1,8 +0,0 @@
-package com.gobravery.wind.server;
-
-import java.io.IOException;
-
-public interface UploadService {
-	public void acceptFile(byte[] file,String name)throws IOException;
-
-}
--- src/test/java/com/gobravery/wind/xml/XmlTest.java
+++ src/test/java/com/gobravery/wind/xml/XmlTest.java
@@ -1,5 +1,7 @@
 package com.gobravery.wind.xml;
 
+import com.gobravery.wind.common.xml.ValidateXMLDTD;
+
 public class XmlTest {
 	@org.junit.Test  
 	public void testValidate(){
